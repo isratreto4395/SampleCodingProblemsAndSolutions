4ffc50f6c70e47b9cf9d70414d30518378f6d8d1
isratreto4395 <isratreto4395@gmail.com>
2021-01-12 23:44:21 -0500

    "New Problems"

---
+++ src/maxOfTwoNumbers/MainMethod.java
@@ -0,0 +1,8 @@
+package maxOfTwoNumbers;
+
+public class MainMethod {
+    public static void main(String[] args) {
+        MaxOf2Num.max(6,65);
+
+    }
+}
---
+++ src/maxOfTwoNumbers/MaxOf2Num.java
@@ -0,0 +1,15 @@
+package maxOfTwoNumbers;
+
+public class MaxOf2Num {
+
+    public static void max(int num1, int num2) {
+        if (num1 > num2) {
+            System.out.println("The max is : " + num1);
+        } else {
+            System.out.println("The max is : " + num2);
+        }
+
+
+    }
+}
+
