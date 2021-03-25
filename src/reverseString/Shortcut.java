f21ef535fd2108c664e445a058453231ad150eff
isratreto4395 <isratreto4395@gmail.com>
2021-03-25 07:27:15 -0500

    commit

--- src/reverseString/Shortcut.java
+++ src/reverseString/Shortcut.java
@@ -5,7 +5,7 @@ public class Shortcut {
     public static void main(String[] args) {
 
         String s = "Israt Reto";
-        StringBuffer e = new StringBuffer(s);
+        StringBuffer e = new StringBuffer(s);// stringBuffer is a peer class of string that provides much of the functionality of strings
         System.out.println(e.reverse());
     }
 
